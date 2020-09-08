337fbc9d378b6aadcd887484f9ce852f4fbeeeb3
Nati <natan.kamusher@gmail.com>
2020-09-08 17:34:11 +0300

    Updated user details

--- src/js/components/UserDetails.js
+++ src/js/components/UserDetails.js
@@ -11,6 +11,20 @@ const UserDetails = () => {
         <label htmlFor="username">User Name:</label>
         <span id="username">avi@gmail.com</span>
       </div>
+      <div>
+        <label htmlFor="timeout">Session time out (Minutes):</label>
+        <span id="username">44</span>
+      </div>
+      <div>
+        <label htmlFor="created">Created Date:</label>
+        <span id="username">11/12/1998</span>
+      </div>
+      <div>
+        <label htmlFor="permissions">Permissions:</label>
+        <span id="username">View Subscriptions, Edit Movies</span>
+      </div>
+      <input type="button" value="Edit" onClick={()=>{}} />
+      <input type="button" value="Delete" onClick={()=>{}} />
     </div>
   )
 }
